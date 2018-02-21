80dfb4ebf830485c6178a90a49e7f8ed5bfec528
Nat! <nat@mulle-kybernetik.com>
2018-02-21 22:38:19 +0100

    properly mirror version into extensions

--- mulle-project/version-info.sh
+++ mulle-project/version-info.sh
@@ -6,3 +6,5 @@
 #
 VERSIONFILE=mulle-sde
 VERSIONNAME=MULLE_EXECUTABLE_VERSION
+VERSIONFILEMIRRORS="src/extensions/mulle-sde/extension/etc/version/mulle-sde/extension
+src/extensions/mulle-sde/sde/etc/version/mulle-sde/sde"
